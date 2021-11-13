b1a0900e06f459e4d68723487cd06b95757250a5
DreamEcho100 <maze6572198@gmail.com>
2021-11-13 23:57:13 +0200

    - init.

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+// const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
+	const envVars = {
+		reactStrictMode: true,
+		eslint: {
+			// Warning: This allows production builds to successfully complete even if
+			// your project has ESLint errors.
+			ignoreDuringBuilds: true,
+		},
+		env: {
+			// EXAMPLE: process.env.EXAMPLE,
+		},
+	};
+
+	// if (phase === PHASE_DEVELOPMENT_SERVER) {
+	// } else {
+	// }
+
+	return envVars;
+};
